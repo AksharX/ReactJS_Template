03daed84234af38ad0f676fa470602337387412e
AksharX <ap39058n@pace.edu>
2018-07-24 22:18:48 -0400

    Added Schema's, Security Libraries

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const app            = express();
 let   db             = require('./config/db');
 const path           = require('path');
 
-
-
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,14 +13,10 @@ MongoClient.connect(db.url, {useNewUrlParser:true}, (err, database) => {
   if (err) return console.log(err)
   
   db = database.db("akshar_patel_database")
-  require('./server/app/routes')(app, db);
+  require('./server/routes')(app, db);
 
   app.listen(port, () => {
     console.log('We are live on ' + port);
   });               
 })
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-});
-app.use(express.static("build"));
 
---
+++ server/routes/note_routes.js
@@ -0,0 +1,21 @@
+const User = require("../models/user");
+
+
+module.exports = function(app, db) {
+    app.get('/api/:id', (req, res) => {
+      
+    });
+
+    
+    app.post('/api', (req, res) => {
+      const note = { text: req.body.body, title: req.body.title };
+      db.collection('notes').insert(note, (err, result) => {
+        if (err) { 
+          res.send({ 'error': 'An error has occurred' }); 
+        } else {
+          res.send(result.ops[0]);
+        }
+      });
+    });
+  };
+  
